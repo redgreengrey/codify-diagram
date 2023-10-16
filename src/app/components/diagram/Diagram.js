ce590326fa9376550ecc8347cd2ae8bf23555a75
v.dudin <v.dudin@infobis>
2023-10-16 23:11:06 +0400

    add background and border-radius to chart

--- src/app/components/diagram/Diagram.js
+++ src/app/components/diagram/Diagram.js
@@ -21,71 +21,74 @@ const Diagram = () => {
                 value={val}
                 onChange={onChange}
                 options={dropdownOptions}/>
-            <HighchartsReact
-                highcharts={Highcharts}
-                options={{
-                    chart: {
-                        width: 1000,
-                        height: 400,
-                    },
-                    title: {
-                        text: '',
-                    },
-                    series: [{
-                        data: currentData,
-                        type: 'column',
-                    }],
-                    xAxis: {
-                        lineColor: "transparent",
-                        categories: fakeChartData[val],
-                        fontSize: 20,
-                        tickLength: 0,
-                        labels: {
-                            step: 1,
-                            style: {
-                                font: '20px Manrope, sans-serif'
-                            },
-                        }
-                    },
-                    yAxis: {
-                        title: '',
-                        gridLineWidth: 0,
-                        offset: 50,
-                        labels: {
-                            format: '{value}',
-                            style: {
-                                font: '20px Manrope, sans-serif',
-                            },
-                            align: 'left',
-                            distance: 10,
-                        }
-                    },
-                    tooltip: {
-                        backgroundColor: '#65FF8E',
-                        shape: "square",
-                        style: {
-                            padding: 0
+            <div className={style.chart}>
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    options={{
+                        chart: {
+                            backgroundColor: '#FFF2FE',
+                            width: 1000,
+                            height: 400,
                         },
-                        formatter: function () {
-                            return '<span>' + this.y + '</span>'
-                        }
-                    },
-                    legend: {
-                        enabled: false,
-                    },
-                    credits: {
-                        enabled: false
-                    },
-                    borderWidth: 0,
+                        title: {
+                            text: '',
+                        },
+                        series: [{
+                            data: currentData,
+                            type: 'column',
+                        }],
+                        xAxis: {
+                            lineColor: "transparent",
+                            categories: fakeChartData[val],
+                            fontSize: 20,
+                            tickLength: 0,
+                            labels: {
+                                step: 1,
+                                style: {
+                                    font: '20px Manrope, sans-serif'
+                                },
+                            }
+                        },
+                        yAxis: {
+                            title: '',
+                            gridLineWidth: 0,
+                            offset: 50,
+                            labels: {
+                                format: '{value}',
+                                style: {
+                                    font: '20px Manrope, sans-serif',
+                                },
+                                align: 'left',
+                                distance: 10,
+                            }
+                        },
+                        tooltip: {
+                            backgroundColor: '#65FF8E',
+                            shape: "square",
+                            style: {
+                                padding: 0
+                            },
+                            formatter: function () {
+                                return '<span>' + this.y + '</span>'
+                            }
+                        },
+                        legend: {
+                            enabled: false,
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        borderWidth: 0,
 
-                    plotOptions: {
-                        series: {
-                            pointWidth: 16,
-                            color: '#000AFF',
-                        }
-                    },
-                }}
-            />
+                        plotOptions: {
+                            series: {
+                                pointWidth: 16,
+                                color: '#000AFF',
+                            }
+                        },
+                    }}
+                />
+            </div>
         </div>
     )
 }
